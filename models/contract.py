f2e07417f5b179a4dc5684e4ee52a6c3fd78a03f
CatherineAmir <catherine.amir20@gmail.com>
2026-04-02 10:37:28 +0200

    fix moudle warning

--- models/contract.py
+++ models/contract.py
@@ -4,4 +4,4 @@ from odoo import fields, models, api
 class HrContract(models.Model):
     _inherit="hr.contract"
 
-    secondary_currency_id=fields.Many2one("res.currency",related="structure_type_id.default_struct_id.currency_id",store=True)
+    secondary_currency_id=fields.Many2one("res.currency",related="structure_type_id.default_struct_id.currency_id",store=True,string="Secondary Currency")
--- models/hr_payslip.py
+++ models/hr_payslip.py
@@ -4,7 +4,7 @@ from odoo import fields, models, api
 class HrPayslip(models.Model):
     _inherit = 'hr.payslip'
 
-    secondary_currency_id=fields.Many2one("res.currency", string="Currency",related="struct_id.currency_id",store=True)
+    secondary_currency_id=fields.Many2one("res.currency", string="Secondary Currency",related="struct_id.currency_id",store=True)
     currency_rate=fields.Float(string="Currency Rate",default=1.0,readonly=True)
 
 
@@ -78,16 +78,6 @@ class HrPayslip(models.Model):
         for slip in self:
            for line in slip.line_ids:
                line.secondary_amount = line.total / slip.currency_rate
-            # rules = slip.line_ids
-            # for line in lines:
-            # rule_ids = line.filtered(lambda rule: rule.code in rule.salary_rule_id.condition_python)
-            #     if rule_ids:
-            # for rule_id in rules:
-            #     rule_id.secondary_currency = True
-            #     rule_id.secondary_currency_id = line.secondary_currency_id
-            #     rule_id.secondary_amount = rule_id.total / line.exchange_rate
-            #     rule_id.exchange_rate = line.exchange_rate
-
 
 
 
@@ -98,7 +88,7 @@ class HrPayslip(models.Model):
 class PayslipLine(models.Model):
     _inherit = "hr.payslip.line"
 
-    secondary_currency=fields.Boolean(string="Secondary Currency",compute="_compute_is_secondary_currency",store=True)
+    secondary_currency=fields.Boolean(string="Is Secondary Currency ?",compute="_compute_is_secondary_currency",store=True)
     secondary_currency_id=fields.Many2one('res.currency', string="Secondary Currency",related="slip_id.secondary_currency_id",store=True)
     secondary_amount=fields.Monetary(string="Secondary Amount", currency_field='secondary_currency_id')
     exchange_rate=fields.Float(string="Exchange Rate", related="slip_id.currency_rate",store=True)
